d8a9ac04a13c077f1f1dbca60948ed93562d0568
Victor Magela <victormateusamato@outlook.com>
2026-02-23 14:32:08 -0300

    Implements the javascript functions to resize the grid

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 let gridCount = 16;
-let containerArea = Math.pow(gridCount, 2);
 
 const container = document.querySelector(".container");
+const sizeButton = document.querySelector("#size-btn");
 
-for (let i = 0; i < containerArea; i++) {
+run();
+
+function run() {
+
+    let containerArea = Math.pow(gridCount, 2);
+
+    for (let i = 0; i < containerArea; i++) {
+        
+        let gridSquare = document.createElement("div");
     
-    let gridSquare = document.createElement("div");
-
-    gridSquare.style.flex = `1 1 ${(100 / gridCount).toString()}%`;
-
-    container.appendChild(gridSquare);
-
-    gridSquare.addEventListener("mouseenter", () => {
-        gridSquare.style.backgroundColor = "black";
-    });
+        gridSquare.style.flex = `1 1 ${(100 / gridCount).toString()}%`;
+    
+        container.appendChild(gridSquare);
+    
+        gridSquare.addEventListener("mouseenter", () => {
+            gridSquare.style.backgroundColor = "black";
+        });
+    }
 }
 
+
+function changeGridSize() {
+    let size = Number(prompt("How many squares per side do you want? Min: 16 Max: 100"));
+
+    if (size < 16 || size > 100 || isNaN(size)) {
+        alert("Invalid size!");
+        return;
+    }
+
+    container.replaceChildren();
+
+    gridCount = size;
+
+    run();
+};
